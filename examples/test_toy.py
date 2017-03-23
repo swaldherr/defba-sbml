52713aa27ad8b1cca7d6117d73b8ba7ef9f8e001
Henning Lindhorst <henning.lindhorst@ovgu.de>
2017-03-23 17:16:25 +0100

    added a Maintenance reaction

---
+++ examples/test_toy.py
@@ -0,0 +1,7 @@
+import numpy as np
+import defba
+model_object = defba.readSBML('resalloc1_fbc.xml')
+#solution2 = deFBA_net.deFBA(RBA_init=False, Tend=1, steps=2)
+
+#import brn
+#deFBA_net = brn.fromSBML('resalloc1_fbc.xml', enzyme_extension=True, cached=False, reduce_vars=False, enzymes_included=True)
